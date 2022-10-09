3b74e5560c3e0fd6cb0705d0724b82f09d7987a6
MishanyaZh <76045588+MishanyaZh@users.noreply.github.com>
2022-10-09 22:48:48 +0200

    add actions

--- src/components/IrregularVerbsList/IrregularVerbsList.jsx
+++ src/components/IrregularVerbsList/IrregularVerbsList.jsx
@@ -1,59 +1,130 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { irregularVerbs } from '../../data/irregularVerbs';
 import {
-  Ul,
-  Li,
-  Word,
-  Box,
+  Container,
   StyledTableHead,
   StyledTableCellHead,
   StyledTableCell,
+  StyledTableRow,
+  Actions,
 } from './IrregularVerbsList.styled';
+
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-// import TableCell, { tableCellClasses } from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import { styled } from '@mui/material/styles';
 
-const StyledTableRow = styled(TableRow)(({ theme }) => ({
-  '&:nth-of-type(odd)': {
-    backgroundColor: theme.palette.action.hover,
-  },
-  // hide last border
-  '&:last-child td, &:last-child th': {
-    border: 0,
-  },
-}));
+import FormControl from '@mui/material/FormControl';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Switch from '@mui/material/Switch';
+
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 
 const IrregularVerbsList = () => {
+  const [data, setData] = useState(irregularVerbs);
+  const [inputValue, setInputValue] = useState('');
+  const [filteredData, setFilteredData] = useState(data);
+
+  const getFilteredData = e => {
+    const target = e.target.value;
+    setInputValue(target);
+    const filter = data.filter(v =>
+      v.base.toLocaleLowerCase().includes(target.toLocaleLowerCase()),
+    );
+    setFilteredData(filter);
+  };
+
+  const [state, setState] = useState({
+    verbs: true,
+    translate: true,
+  });
+
+  const handleChange = event => {
+    setState({
+      ...state,
+      [event.target.name]: event.target.checked,
+    });
+  };
+
   return (
-    <Box>
+    <Container>
+      <Actions>
+        <FormControl component="fieldset" variant="standard">
+          <FormGroup>
+            <FormControlLabel
+              control={
+                <Switch
+                  checked={state.verbs}
+                  onChange={handleChange}
+                  name="verbs"
+                />
+              }
+              label="verbs"
+            />
+            <FormControlLabel
+              control={
+                <Switch
+                  checked={state.translate}
+                  onChange={handleChange}
+                  name="translate"
+                />
+              }
+              label="translate"
+            />
+          </FormGroup>
+        </FormControl>
+        <Box
+          component="form"
+          sx={{
+            '& > :not(style)': { m: 1, width: '25ch' },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <TextField
+            onChange={getFilteredData}
+            value={inputValue}
+            id="standard-search"
+            label="Search Verbs"
+            type="search"
+            variant="outlined"
+          />
+        </Box>
+      </Actions>
       <TableContainer component={Paper}>
         <Table aria-label="customized table">
           <StyledTableHead>
             <TableRow>
-              <StyledTableCellHead>base</StyledTableCellHead>
-              <StyledTableCellHead>pastSimple</StyledTableCellHead>
-              <StyledTableCellHead>pastParticiple</StyledTableCellHead>
-              <StyledTableCellHead>translate</StyledTableCellHead>
+              <StyledTableCellHead>Base</StyledTableCellHead>
+              <StyledTableCellHead>Past Simple</StyledTableCellHead>
+              <StyledTableCellHead>Past Participle</StyledTableCellHead>
+              <StyledTableCellHead>Translate</StyledTableCellHead>
             </TableRow>
           </StyledTableHead>
           <TableBody>
-            {irregularVerbs.map((verb, index) => (
+            {filteredData.map((verb, index) => (
               <StyledTableRow key={index}>
-                <StyledTableCell>{verb.base}</StyledTableCell>
-                <StyledTableCell>{verb.pastSimple}</StyledTableCell>
-                <StyledTableCell>{verb.pastParticiple}</StyledTableCell>
-                <StyledTableCell>translate</StyledTableCell>
+                <StyledTableCell>
+                  {state.verbs ? verb.base : '----------'}
+                </StyledTableCell>
+                <StyledTableCell>
+                  {state.verbs ? verb.pastSimple : '----------'}
+                </StyledTableCell>
+                <StyledTableCell>
+                  {state.verbs ? verb.pastParticiple : '----------'}
+                </StyledTableCell>
+                <StyledTableCell>
+                  {state.translate ? verb.translate : '------'}
+                </StyledTableCell>
               </StyledTableRow>
             ))}
           </TableBody>
         </Table>
       </TableContainer>
-    </Box>
+    </Container>
   );
 };
 
