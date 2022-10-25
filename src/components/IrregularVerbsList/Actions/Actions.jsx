b595fc68ab7aa101871459660063a903ad1f35c4
MishanyaZh <76045588+MishanyaZh@users.noreply.github.com>
2022-10-25 18:00:36 +0200

    gor git lern create Actions

--- src/components/IrregularVerbsList/Actions/Actions.jsx
+++ src/components/IrregularVerbsList/Actions/Actions.jsx
@@ -1,7 +1,61 @@
 import React from 'react';
+import FormControl from '@mui/material/FormControl';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Switch from '@mui/material/Switch';
 
-const Actions = () => {
-  return <div>Actions</div>;
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import { Wrapper } from './Actions.styled';
+
+const Actions = ({
+  getFilteredData,
+  inputValue,
+  handleChange,
+  verbs,
+  translate,
+}) => {
+  return (
+    <Wrapper>
+      <Box
+        component="form"
+        sx={{
+          '& > :not(style)': { m: 1 },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField
+          onChange={getFilteredData}
+          value={inputValue}
+          id="standard-search"
+          label="Search Verbs"
+          type="search"
+          variant="outlined"
+        />
+      </Box>
+      <FormControl component="fieldset" variant="standard">
+        <FormGroup>
+          <FormControlLabel
+            control={
+              <Switch checked={verbs} onChange={handleChange} name="verbs" />
+            }
+            label="verbs"
+          />
+          <FormControlLabel
+            control={
+              <Switch
+                checked={translate}
+                onChange={handleChange}
+                name="translate"
+              />
+            }
+            label="translate"
+          />
+        </FormGroup>
+      </FormControl>
+    </Wrapper>
+  );
 };
 
 export default Actions;
--- src/components/IrregularVerbsList/Actions/Actions.styled.jsx
+++ src/components/IrregularVerbsList/Actions/Actions.styled.jsx
@@ -1,1 +1,12 @@
 import styled from '@emotion/styled';
+export const Wrapper = styled.div`
+  position: sticky;
+  top: 0;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding-right: 8px;
+  background-color: #8a9599;
+  border-radius: 5px;
+`;
