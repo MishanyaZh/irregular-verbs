ac3276f91e59fd3751278436786e28cfb1a2017b
MishanyaZh <76045588+MishanyaZh@users.noreply.github.com>
2022-11-02 18:55:52 +0100

    hot fix Button style

--- src/components/IrregularVerbsList/Actions/ScrollButton/ScrollButton.styled.jsx
+++ src/components/IrregularVerbsList/Actions/ScrollButton/ScrollButton.styled.jsx
@@ -8,24 +8,20 @@ export const BottomBox = styled.div`
   height: 20px;
   font-size: 3rem;
   z-index: 1;
-  opacity: 0.7;
-  :hover,
-  :focus,
-  :active {
-    opacity: 1;
-  }
 `;
 
 export const Button = styled.button`
   border-radius: 50%;
   border: 1px solid #1976d2;
   color: #1976d2;
-  background-color: rgb(55, 135, 214);
+  background-color: rgb(136, 175, 214);
   cursor: pointer;
+
   :hover,
   :focus,
   :active {
     color: yellowgreen;
     border-color: yellowgreen;
+    background-color: #1976d2;
   }
 `;
